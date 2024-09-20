c1d0348813b629c24c7a3e5b4588367d51302d5e
Danielle73 <daniellelee73@gmail.com>
2024-09-20 17:33:15 +0100

    function created for testing

--- learn-unit-testing/workshop/index.test.js
+++ learn-unit-testing/workshop/index.test.js
@@ -2,3 +2,13 @@ test('updatingURL', () => {
     equal(makeUrl("pikachu"), "https://pokeapi.co/api/v2/pikachu");
     equal(makeUrl("bulbasaur"), "https://pokeapi.co/api/v2/bulbasaur");
 });
+
+function test(name, testFunction){
+    try {
+        testFunction();
+        console.log('Test Passed');
+    } catch(error){
+        console.error('Test Failed')
+    }
+
+}
